6f28cd7454d0a5a18f03d65ce3dd36e59ac7844f
SarahGathoni <sarah.wanjiku.gathoni@gmail.com>
2023-05-13 23:02:27 -0700

    changes

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,16 @@
 # Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
 
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+# Enable the virtual host
+exec { 'enable-wordpress':
+  command     => 'a2ensite wordpress.conf',
+  refreshonly => true,
+  path        => '/usr/sbin:/usr/bin',
+  subscribe   => File['/etc/apache2/sites-available/wordpress.conf'],
+}
+
+# Restart Apache service
+service { 'apache2':
+  ensure  => running,
+  enable  => true,
+  require => [Package['apache2'], Exec['enable-wordpress']],
 }
